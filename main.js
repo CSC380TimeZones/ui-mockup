cd82335e3e58e0ce1f22c179436f32a9a0125eaa
aaradhyaach <98428989+aaradhyaach@users.noreply.github.com>
2023-03-27 10:41:01 -0400

    added main.js
    still has an error where after a timeslot gets added it pops up over other divs

---
+++ main.js
@@ -0,0 +1,107 @@
+const timeslot = document.getElementById("timeslot");
+const addBtn = document.getElementById("add-timeslot");
+
+const timeslotSub = document.getElementById("subopt-timeslot");
+const addBtnSub = document.getElementById("add-subopt-timeslot");
+
+addBtn.addEventListener("click", function() {
+    const weekdays = document.createElement("div");
+    weekdays.classList.add("weekdays");
+    weekdays.innerHTML = `
+    <div class="timeslot">
+            <button class="sub" id="deleteBtn">-</button>
+            <div class="time-range">
+              <input
+                class="small"
+                type="text"
+                id="start-time"
+                name="start-time"
+                value="10:00"
+              />
+              -
+              <input
+                class="small"
+                type="text"
+                id="end-time"
+                name="end-time"
+                value="4:00"
+              />
+            </div>
+            <div class="weekdays">
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" checked />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" checked />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" />
+              <span class="label">s</span>
+              <span class="label">m</span>
+              <span class="label">t</span>
+              <span class="label">w</span>
+              <span class="label">t</span>
+              <span class="label">f</span>
+              <span class="label">s</span>
+            </div>
+          </div>
+        </div>
+    `;
+    timeslot.appendChild(weekdays);
+  
+    const deleteBtn = weekdays.querySelector("#deleteBtn");
+    deleteBtn.addEventListener("click", function() {
+      timeslot.removeChild(weekdays);
+    });
+  });
+
+
+addBtnSub.addEventListener("click", function() {
+    const weekdays = document.createElement("div");
+    weekdays.classList.add("weekdays");
+    weekdays.innerHTML = `
+    <div class="subopt-timeslot">
+            <button class="sub" id="deleteBtn">-</button>
+            <div class="time-range">
+              <input
+                class="small"
+                type="text"
+                id="start-time"
+                name="start-time"
+                value="10:00"
+              />
+              -
+              <input
+                class="small"
+                type="text"
+                id="end-time"
+                name="end-time"
+                value="4:00"
+              />
+            </div>
+            <div class="weekdays">
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" checked />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" checked />
+              <input type="checkbox" class="weekday" />
+              <input type="checkbox" class="weekday" />
+              <span class="label">s</span>
+              <span class="label">m</span>
+              <span class="label">t</span>
+              <span class="label">w</span>
+              <span class="label">t</span>
+              <span class="label">f</span>
+              <span class="label">s</span>
+            </div>
+          </div>
+        </div>
+    `;
+    timeslotSub.appendChild(weekdays);
+  
+    const deleteBtn = weekdays.querySelector("#deleteBtn");
+    deleteBtn.addEventListener("click", function() {
+      timeslotSub.removeChild(weekdays);
+    });
+  });
+  
