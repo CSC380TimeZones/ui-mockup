95c65e21aeeea4265107ee7382ec70cec6387286
clxxiii <clxxiii@users.noreply.github.com>
2023-03-27 11:47:40 -0400

    fix add and remove buttons

--- main.js
+++ main.js
@@ -5,61 +5,56 @@ const timeslotSub = document.getElementById("suboptimal-availabilities");
 const addBtnSub = document.getElementById("add-subopt-timeslot");
 
 addBtn.addEventListener("click", function() {
-    const weekdays = document.createElement("div");
-    weekdays.classList.add("weekdays");
-    weekdays.innerHTML = `
-    <div class="timeslot">
-            <button class="sub" id="deleteBtn">-</button>
-            <div class="time-range">
-              <input
-                class="small"
-                type="text"
-                id="start-time"
-                name="start-time"
-                value="10:00"
-              />
-              -
-              <input
-                class="small"
-                type="text"
-                id="end-time"
-                name="end-time"
-                value="4:00"
-              />
-            </div>
-            <div class="weekdays">
-              <input type="checkbox" class="weekday" />
-              <input type="checkbox" class="weekday" />
-              <input type="checkbox" class="weekday" checked />
-              <input type="checkbox" class="weekday" />
-              <input type="checkbox" class="weekday" checked />
-              <input type="checkbox" class="weekday" />
-              <input type="checkbox" class="weekday" />
-              <span class="label">s</span>
-              <span class="label">m</span>
-              <span class="label">t</span>
-              <span class="label">w</span>
-              <span class="label">t</span>
-              <span class="label">f</span>
-              <span class="label">s</span>
-            </div>
+  const weekdays = document.createElement("div");
+  weekdays.classList.add("timeslot");
+  weekdays.innerHTML = `
+          <button class="sub" id="deleteBtn">-</button>
+          <div class="time-range">
+            <input
+              class="small"
+              type="text"
+              id="start-time"
+              name="start-time"
+              value="10:00"
+            />
+            -
+            <input
+              class="small"
+              type="text"
+              id="end-time"
+              name="end-time"
+              value="4:00"
+            />
+          </div>
+          <div class="weekdays">
+            <input type="checkbox" class="weekday" />
+            <input type="checkbox" class="weekday" />
+            <input type="checkbox" class="weekday" checked />
+            <input type="checkbox" class="weekday" />
+            <input type="checkbox" class="weekday" checked />
+            <input type="checkbox" class="weekday" />
+            <input type="checkbox" class="weekday" />
+            <span class="label">s</span>
+            <span class="label">m</span>
+            <span class="label">t</span>
+            <span class="label">w</span>
+            <span class="label">t</span>
+            <span class="label">f</span>
+            <span class="label">s</span>
           </div>
         </div>
     `;
-    timeslot.appendChild(weekdays);
-  
-    const deleteBtn = weekdays.querySelector("#deleteBtn");
-    deleteBtn.addEventListener("click", function() {
-      timeslot.removeChild(weekdays);
-    });
-  });
+  timeslot.appendChild(weekdays);
 
+  const deleteBtn = weekdays.querySelector("#deleteBtn");
+  deleteBtn.addEventListener("click",
+                             function() { timeslot.removeChild(weekdays); });
+});
 
 addBtnSub.addEventListener("click", function() {
-    const weekdays = document.createElement("div");
-    weekdays.classList.add("weekdays");
-    weekdays.innerHTML = `
-    <div class="subopt-timeslot">
+  const weekdays = document.createElement("div");
+  weekdays.classList.add("timeslot");
+  weekdays.innerHTML = `
             <button class="sub" id="deleteBtn">-</button>
             <div class="time-range">
               <input
@@ -94,14 +89,11 @@ addBtnSub.addEventListener("click", function() {
               <span class="label">f</span>
               <span class="label">s</span>
             </div>
-          </div>
         </div>
     `;
-    timeslotSub.appendChild(weekdays);
-  
-    const deleteBtn = weekdays.querySelector("#deleteBtn");
-    deleteBtn.addEventListener("click", function() {
-      timeslotSub.removeChild(weekdays);
-    });
-  });
-  
+  timeslotSub.appendChild(weekdays);
+
+  const deleteBtn = weekdays.querySelector("#deleteBtn");
+  deleteBtn.addEventListener("click",
+                             function() { timeslotSub.removeChild(weekdays); });
+});
